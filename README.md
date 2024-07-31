95ff750adf2616f9ca66a9bd58226178577ade28
Daniel Dickison <git@danieldickison.com>
2024-07-30 20:06:06 -0700

    More tests and docs

---
+++ README.md
@@ -0,0 +1,3 @@
+# slug_intl
+
+Provides a simple [slugify](fn/slugify) function to create a String suitable for use as a URL path component. Unlike other similar libraries, this maintains non-ASCII Unicode characters, which are well-supported by current browsers using percent-encoding.
--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,11 @@
+#![doc = include_str!("../README.md")]
+
 use finl_unicode::categories::{CharacterCategories, MajorCategory};
 use unicode_normalization::UnicodeNormalization;
 
-#[derive(Debug)]
-pub struct SlugOptions {
-    min_len: usize,
-    max_len: usize,
-}
-
-impl Default for SlugOptions {
-    fn default() -> Self {
-        Self {
-            min_len: 3,
-            max_len: 30,
-        }
-    }
-}
-
-/// Converts `str` to a `String` suitable for use as a URL path component. It first normalizes
-/// Unicode as NFC, then makes some aesthetic conversions:
+/// Converts `str` to a `String` suitable for use as a URL path component.
+///
+/// It first normalizes Unicode as NFC, then makes some aesthetic conversions:
 /// * letters are lowercased where applicable
 /// * whitespace and punctuation are replaced with hyphen
 /// * repeated hyphens are collapsed
@@ -27,20 +15,35 @@ impl Default for SlugOptions {
 /// supported by current browsers using percent-encoding. Percent-encoding is left as an exercise
 /// for the caller (because you likely want to store the raw UTF-8 in your database, say).
 ///
-/// # Examples
+/// ## Examples
 ///
+/// ASCII only deals mainly with capitalization, punctuation, and whitespace:
 /// ```rust
 /// # use slug_intl::slugify;
-/// assert_eq!(slugify("hello"), "hello".to_string());
-/// assert_eq!(slugify("hello world"), "hello-world".to_string());
-/// assert_eq!(slugify("hello world!"), "hello-world".to_string());
-/// assert_eq!(slugify("hello --world!!!"), "hello-world".to_string());
+/// assert_eq!("hello".to_string(), slugify("Hello"));
+/// assert_eq!("hello-world", slugify("Hello World"));
+/// assert_eq!("hello-world", slugify("Hello World!"));
+/// assert_eq!("hello-world", slugify("/?&#Hello\n\r\n\r   --World!!!"));
 /// ```
+///
+/// Unicode characters are normalized but otherwise preserved:
+/// ```rust
+/// # use slug_intl::slugify;
+/// assert_eq!("おはよう-世界", slugify("おはよう、世界！！"));
+/// assert_eq!("おはよう🐠", slugify("おはよう🐠"));
+/// /// Hyphen replacement is based on what Unicode considers "punctuation":
+/// assert_eq!("1≈3∞5=-£9", slugify("¡¡1≈3∞5=¶£9!!"));
+/// /// Unicode is normalized as NFC
+/// assert_eq!("am\u{00e9}lie", slugify("ame\u{0301}lie"));
+/// ```
+///
+/// You may want to percent-escape the output when rendering HTML:
+/// ```rust
+/// # use slug_intl::slugify;
+/// assert_eq!("hello-%F0%9F%90%A0", urlencoding::encode(&slugify("Hello 🐠")));
+/// ```
+///
 pub fn slugify(str: &str) -> String {
-    slugify_with_opts(str, &SlugOptions::default())
-}
-
-pub fn slugify_with_opts(str: &str, opts: &SlugOptions) -> String {
     let mut prev_hyphen = true; // removes leading hyphens by starting true
 
     let mut process_char = |c: char| match c.get_major_category() {
